d6d162256adb598b7770a5282c088476f92db009
Gustavo <gpontesf06@gmail.com>
2022-12-11 18:00:02 -0300

    Finalizado; Batalha naval sem tratamento de erros de entrada de dados; Sistema de vida para cada navio, sendo obrigatório atingir 2 vezes para destruir;

--- BatalhaNavalSimples/Program.cs
+++ BatalhaNavalSimples/Program.cs
@@ -6,12 +6,16 @@
         {
             Tabuleiros tabuleiroManager = new Tabuleiros();
             bool decidindoNavios = true;
+            Console.WriteLine("\t\tBatalha Naval");
             Console.WriteLine("Desejam jogar com quantos navios?");
             int quantNavios = int.Parse(Console.ReadLine());
             do
             {
-                DesenhaTabuleiro(tabuleiroManager);
+                Console.Clear();
+                Console.WriteLine($"\tJogador {tabuleiroManager.jogadorAtual}");
+                Console.WriteLine($"Jogador {tabuleiroManager.jogadorAtual} pronto? Clique qualquer botão para continuar");
                 Console.ReadKey();
+                DesenhaTabuleiro(tabuleiroManager);
                 int quantidadeNavios = quantNavios;
                 do
                 {
@@ -22,32 +26,51 @@
                     quantidadeNavios--;
                 } while (quantidadeNavios > 0);
 
-                if (tabuleiroManager.jogadorAtual == 1) tabuleiroManager.jogadorAtual = 2;
+                if (tabuleiroManager.jogadorAtual == 1) tabuleiroManager.trocarJogador();
                 else decidindoNavios = false;
             } while (decidindoNavios);
-            
+
+            tabuleiroManager.jogadorAtual = 1;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Vez do jogador {tabuleiroManager.jogadorAtual}. Preparado?");
+                Console.ReadKey();
+                DesenhaTabuleiro(tabuleiroManager);
+                Console.WriteLine("Deseja atacar em qual posição? (ex: 1a)");
+                string posicaoNavio = Console.ReadLine();
+                tabuleiroManager.atacarNavio(int.Parse(posicaoNavio.Substring(0, 1)) - 1, posicaoLetra(posicaoNavio.Substring(1, 1)));
+                Console.WriteLine("ATACANDO!!!");
+                Console.ReadKey();
+                tabuleiroManager.verificaVencedor();
+            } while (!tabuleiroManager.vencedor);
+
+            Console.WriteLine($"Parabéns, jogador {tabuleiroManager.jogadorAtual}!!!! Você venceu!");
         }
 
         static void DesenhaTabuleiro(Tabuleiros tabuleiroManager)
         {
             int escreve = 1;
+            int left = 3;
             Console.Clear();
-            Console.WriteLine($"\tJogador {tabuleiroManager.jogadorAtual}");
+            Console.WriteLine($"Jogador {tabuleiroManager.jogadorAtual}:");
             Console.WriteLine("   A   B   C   D   E   F   G   H");
             foreach (Navio i in tabuleiroManager.getTabuleiroAtual())
             {
-                if (i!=null) Console.Write($"   O");
-                else Console.Write($"   x");
+                Console.CursorLeft = left;
+                if (i!=null) Console.Write($"{i.Vida}");
+                else Console.Write($"x");
+                left += 4;
                 if (escreve % 8 == 0)
                 {
                     Console.CursorLeft = 0;
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.Write($"{escreve / 8}");
                     Console.WriteLine();
+                    left = 3;
                 }
                 escreve++;
             }
-            Console.WriteLine($"Jogador {tabuleiroManager.jogadorAtual} pronto? Clique qualquer botão para continuar");
         }
 
 
--- BatalhaNavalSimples/Tabuleiros.cs
+++ BatalhaNavalSimples/Tabuleiros.cs
@@ -11,12 +11,42 @@ namespace BatalhaNavalSimples
         public Navio[,] tabuleiro1 = new Navio[8, 8];
         public Navio[,] tabuleiro2 = new Navio[8, 8];
         public int jogadorAtual = 1;
-
+        public bool vencedor = false;
         public Tabuleiros() { }
 
         public void adicionaNavio(int posicao1, int posicao2)
         {
-            getTabuleiroAtual()[posicao1, posicao2] = new Navio(100);
+            getTabuleiroAtual()[posicao1, posicao2] = new Navio(10);
+        }
+
+        public void atacarNavio(int posicao1, int posicao2)
+        {
+            trocarJogador();
+            if (getTabuleiroAtual()[posicao1, posicao2] != null)
+            {
+                getTabuleiroAtual()[posicao1, posicao2].Vida -= 5;
+                if (getTabuleiroAtual()[posicao1, posicao2].Vida <= 0) getTabuleiroAtual()[posicao1, posicao2] = null;
+            }
+        }
+
+        public void verificaVencedor()
+        {
+            int contaBuracos = 0;
+            foreach(Navio tabuleiro in getTabuleiroAtual())
+            {
+                if (tabuleiro == null) contaBuracos++;
+            }
+            if (contaBuracos == getTabuleiroAtual().Length)
+            {
+                trocarJogador();
+                vencedor = true;
+            }
+        }
+
+        public void trocarJogador()
+        {
+            if (jogadorAtual == 1) jogadorAtual = 2;
+            else jogadorAtual = 1;
         }
 
         public Navio[,] getTabuleiroAtual()
@@ -24,6 +54,5 @@ namespace BatalhaNavalSimples
             if(jogadorAtual == 1) return tabuleiro1;
             return tabuleiro2;
         }
-
     }
 }
